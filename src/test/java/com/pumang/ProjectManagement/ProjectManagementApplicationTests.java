6c5cbc9211d7fcd887d5c994448c9d47bd897c5b
Pumang Kirange <kirangespumang03@gmail.com>
2024-10-29 13:54:36 +0530

    Project Management Start

---
+++ src/test/java/com/pumang/ProjectManagement/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pumang.ProjectManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
