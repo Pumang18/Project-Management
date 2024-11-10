7c1278a73afb68d381269268f27cf5beb1c8752d
Pumang Kirange <kirangespumang03@gmail.com>
2024-11-10 13:54:44 +0530

    All Model Except Subscription

--- src/main/java/com/pumang/ProjectManagement/model/User.java
+++ src/main/java/com/pumang/ProjectManagement/model/User.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private Long id;
     private String fullName;
     private String email;
     private String password;
